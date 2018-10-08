1d23e4b10c190eac45cda3cc30921def3a047f27
Amine Abri <amineabri@milexa.com>
2018-10-08 16:35:07 +0100

    Fixes

--- wp-content/themes/twentyseventeen/tests/includes/bootstrap.php
+++ wp-content/themes/twentyseventeen/tests/includes/bootstrap.php
@@ -9,7 +9,7 @@
 if ( class_exists( 'PHPUnit\Runner\Version' ) ) {
 	require_once dirname( __FILE__ ) . '/phpunit6-compat.php';
 }
-
+echo "Boostrap PHP included ";
 if ( defined( 'WP_TESTS_CONFIG_FILE_PATH' ) ) {
 	$config_file_path = WP_TESTS_CONFIG_FILE_PATH;
 } else {
--- wp-content/themes/twentyseventeen/tests/mw/bootstrap.php
+++ wp-content/themes/twentyseventeen/tests/mw/bootstrap.php
@@ -2,27 +2,21 @@
 define('DS', DIRECTORY_SEPARATOR);
 require_once dirname( dirname( __FILE__ ) ) . DS . 'includes'. DS .'functions.php';
 require_once dirname(dirname(__FILE__)) . DS . 'vendor' . DS . 'autoload.php';
+function _manually_load_environment() {
 
-if(function_exists("tests_add_filter")){
-	function _manually_load_environment() {
-		// Add your theme …
-		switch_theme('twentyseventeen');
-		// Update array with plugins to include ...
-		$plugins_to_active = [
-			//"advanced-custom-fields-pro/acf.php",
-			//"contact-form-7/wp-contact-form-7.php",
-			//"elasticsearch-mw/elasticsearch-mw.php",
-			//"woocommerce-xero/woocommerce-xero.php",
-			//"woocommerce/woocommerce.php"
-		];
-		update_option( 'active_plugins', $plugins_to_active );
-	}
-	echo "tests_add_filter exist ";
-	tests_add_filter( 'muplugins_loaded', '_manually_load_environment' );
-
-	require dirname( dirname( __FILE__ ) ) . '/includes/bootstrap.php';
-}else{
-	echo "tests_add_filter doesn't exist ";
+	// Add your theme …
+	switch_theme('twentyseventeen');
+	// Update array with plugins to include ...
+	$plugins_to_active = [
+		//"advanced-custom-fields-pro/acf.php",
+		//"contact-form-7/wp-contact-form-7.php",
+		//"elasticsearch-mw/elasticsearch-mw.php",
+		//"woocommerce-xero/woocommerce-xero.php",
+		//"woocommerce/woocommerce.php"
+	];
+	update_option( 'active_plugins', $plugins_to_active );
 }
+tests_add_filter( 'muplugins_loaded', '_manually_load_environment' );
 
+require dirname( dirname( __FILE__ ) ) . '/includes/bootstrap.php';
 
