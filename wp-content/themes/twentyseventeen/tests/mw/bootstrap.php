2eaa5000b655fb1b4a98d7b32c35a2a51ce9d821
Amine Abri <amineabri@milexa.com>
2018-10-08 10:05:41 +0100

    - Upload new theme

---
+++ wp-content/themes/twentyseventeen/tests/mw/bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+
+define('DS', DIRECTORY_SEPARATOR);
+define('DB', true);
+require_once dirname( dirname( __FILE__ ) ) . DS . 'includes'. DS .'functions.php';
+require_once dirname(dirname(__FILE__)) . DS . 'vendor' . DS . 'autoload.php';
+function _manually_load_environment() {
+
+	// Add your theme …
+	switch_theme('twentyseventeen');
+	// Update array with plugins to include ...
+	$plugins_to_active = [
+		//"advanced-custom-fields-pro/acf.php",
+		//"contact-form-7/wp-contact-form-7.php",
+		//"elasticsearch-mw/elasticsearch-mw.php",
+		//"woocommerce-xero/woocommerce-xero.php",
+		//"woocommerce/woocommerce.php"
+	];
+	update_option( 'active_plugins', $plugins_to_active );
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_environment' );
+
+require dirname( dirname( __FILE__ ) ) . '/includes/bootstrap.php';
---
+++ wp-content/themes/twentyseventeen/tests/mw/frontend/LoginTest.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * to run this test run this command :
+ * phpunit --filter testHasLoginForm
+ * */
+class LoginTest extends PHPUnit_Extensions_Selenium2TestCase {
+
+	public static $browsers = [
+		[
+			'browserName' => 'chrome',
+			'browser'     => '*chrome',
+			'host'        => 'localhost',
+			'port'        => 4444,
+			'timeout'     => 2000,
+		],
+	];
+
+	public function setUp(){
+		$this->setBrowserUrl("https://www.muralswallpaper.co.uk/wp");
+	}
+
+	public function testHasLoginForm(){
+		$this->url("wp-login.php");
+		$username = $this->byName("log");
+		$password = $this->byName("pwd");
+		$this->assertEquals("", $username->value());
+		$this->assertEquals("", $password->value());
+	}
+}
